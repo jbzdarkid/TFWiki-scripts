f94cc0fe8db12174234bddde4da3b994500e2a36
jbzdarkid <jbzdarkid@gmail.com>
2016-02-19 16:30:42 -0500

    I don't know it does stuff

---
+++ master.py
@@ -0,0 +1,18 @@
+from wikitools.page import Page
+from wikitools import wiki
+from getpass import getpass
+w = wiki.Wiki('https://wiki.teamfortress.com/w/api.php')
+username = raw_input('Username: ')
+w.login(username, getpass(), verify=True)
+
+summary = 'Automatic Update by %s using [https://github.com/jbzdarkid/TFWiki-scripts Wikitools]' % username
+
+import wiki_edit_stats, wiki_unused_files, equipregions, external_links_analyse
+
+
+'''
+Page(w, 'Team Fortress Wiki:Reports/Users by edit count').edit(text=wiki_edit_stats.main(), summary=summary)
+p
+'''
+Page(w, 'Team Fortress Wiki:Reports/Unused files').edit(text=wiki_unused_files.main(), summary=summary)
+
