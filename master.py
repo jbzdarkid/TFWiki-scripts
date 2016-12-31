1294ecfb9a7fd4c8ed971d12b9930a18dc13edca
jbzdarkid <jbzdarkid@gmail.com>
2016-12-31 08:30:04 -0500

    Simplify for testing

--- master.py
+++ master.py
@@ -1,6 +1,13 @@
 from wikitools.page import Page
 from wikitools import wiki
-import config
+# import config
+from sys import argv
+
+config = {
+    'wikiApi': 'http://wiki.teamfortress.com/w/api.php',
+    'wikiPassword': argv[2],
+    'wikiUsername': argv[1],
+}
 
 w = wiki.Wiki(config['wikiApi'])
 w.login(config['wikiUsername'], config['wikiPassword'])
@@ -11,6 +18,8 @@ import wiki_edit_stats, wiki_undocumented_templates, wiki_unused_files, equipreg
 
 Page(w, 'Team Fortress Wiki:Reports/Users by edit count').edit(text=wiki_edit_stats.main(), summary=summary)
 
+exit(0)
+
 Page(w, 'Team Fortress Wiki:Reports/Undocumented templates').edit(text=wiki_undocumented_templates.main(), summary=summary)
 
 Page(w, 'Team Fortress Wiki:Reports/Unused files').edit(text=wiki_unused_files.main(), summary=summary)
@@ -18,8 +27,8 @@ Page(w, 'Team Fortress Wiki:Reports/Unused files').edit(text=wiki_unused_files.m
 text = Page(w, 'Template:Equip region table').getWikiText()
 start = text.index('! {{item name|') # Start of equip regions
 end = text.index('<noinclude>') # End of table
-Page(w, 'Template:Equip region table').edit(text=text[:start]+equipregions.main()+text[end:], summary=summary)
+#Page(w, 'Template:Equip region table').edit(text=text[:start]+equipregions.main()+text[end:], summary=summary)
 
-Page(w, 'Template:LODTable').edit(text=LODTables.main(), summary=summary)
+#Page(w, 'Template:LODTable').edit(text=LODTables.main(), summary=summary)
 
 Page(w, 'Team Fortress Wiki:Reports/External links').edit(text=external_links_analyse2.main(), summary=summary)
