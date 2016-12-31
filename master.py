1ccdbd4d6c7c384666508b524bd953ad16f478b4
jbzdarkid <jbzdarkid@gmail.com>
2016-12-31 14:53:33 -0600

    Break up imports so that they can be commented out easier

--- master.py
+++ master.py
@@ -1,7 +1,6 @@
 from wikitools.page import Page
 from wikitools import wiki
 from os import environ
-print environ
 
 # travis encrypt WIKI_USERNAME=pootis -a -x
 # travis encrypt WIKI_PASSWORD=woooki -a -p
@@ -17,21 +16,25 @@ w.login(config['wikiUsername'], config['wikiPassword'])
 
 summary = 'Automatic Update by %s using [https://github.com/jbzdarkid/TFWiki-scripts Wikitools]' % config['wikiUsername']
 
-import wiki_edit_stats, wiki_undocumented_templates, wiki_unused_files, equipregions, external_links_analyse2, LODTables
-
+import wiki_edit_stats
 Page(w, 'Team Fortress Wiki:Reports/Users by edit count').edit(text=wiki_edit_stats.main(), summary=summary)
 
 exit(0)
 
+import wiki_undocumented_templates
 Page(w, 'Team Fortress Wiki:Reports/Undocumented templates').edit(text=wiki_undocumented_templates.main(), summary=summary)
 
+import wiki_unused_files
 Page(w, 'Team Fortress Wiki:Reports/Unused files').edit(text=wiki_unused_files.main(), summary=summary)
 
-text = Page(w, 'Template:Equip region table').getWikiText()
-start = text.index('! {{item name|') # Start of equip regions
-end = text.index('<noinclude>') # End of table
-#Page(w, 'Template:Equip region table').edit(text=text[:start]+equipregions.main()+text[end:], summary=summary)
+# import equipregions
+# text = Page(w, 'Template:Equip region table').getWikiText()
+# start = text.index('! {{item name|') # Start of equip regions
+# end = text.index('<noinclude>') # End of table
+# Page(w, 'Template:Equip region table').edit(text=text[:start]+equipregions.main()+text[end:], summary=summary)
 
-#Page(w, 'Template:LODTable').edit(text=LODTables.main(), summary=summary)
+# import LODTables
+# Page(w, 'Template:LODTable').edit(text=LODTables.main(), summary=summary)
 
+import external_links_analyse2
 Page(w, 'Team Fortress Wiki:Reports/External links').edit(text=external_links_analyse2.main(), summary=summary)
