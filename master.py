aaa739c7407d227f60d78c1797269b470f02038c
jbzdarkid <jbzdarkid@gmail.com>
2016-12-31 18:11:27 -0600

    Dealing with blacklists :(

--- master.py
+++ master.py
@@ -29,4 +29,10 @@ summary = 'Automatic Update by %s using Travis-ci and [https://github.com/jbzdar
 # Page(w, 'Template:LODTable').edit(text=LODTables.main(), summary=summary)
 
 import external_links_analyse2
-print Page(w, 'Team Fortress Wiki:Reports/External links').edit(text=external_links_analyse2.main(), summary=summary)
+external_links = external_links_analyse2.main()
+res = Page(w, 'Team Fortress Wiki:Reports/External links').edit(text=external_links, summary=summary)
+# Dealing with the link blacklist
+if res['edit']['result'] == 'Failure':
+    for url in res['edit']['spamblacklist'].split('|'):
+        external_links.replace(url, url[::-1])
+    res = Page(w, 'Team Fortress Wiki:Reports/External links').edit(text=external_links, summary=summary)
