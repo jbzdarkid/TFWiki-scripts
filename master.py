8b50a875a46a866aa1df32753f8edc92d15cbdb0
jbzdarkid <jbzdarkid@gmail.com>
2017-01-09 12:21:59 -0500

    Remove the scripts that require VDF to run

--- master.py
+++ master.py
@@ -19,14 +19,14 @@ Page(w, 'Team Fortress Wiki:Reports/Undocumented templates').edit(text=wiki_undo
 import wiki_unused_files
 Page(w, 'Team Fortress Wiki:Reports/Unused files').edit(text=wiki_unused_files.main(), summary=summary)
 
-import equipregions
-text = Page(w, 'Template:Equip region table').getWikiText()
-start = text.index('! {{item name|') # Start of equip regions
-end = text.index('<noinclude>') # End of table
-Page(w, 'Template:Equip region table').edit(text=text[:start]+equipregions.main()+text[end:], summary=summary)
+# import equipregions
+# text = Page(w, 'Template:Equip region table').getWikiText()
+# start = text.index('! {{item name|') # Start of equip regions
+# end = text.index('<noinclude>') # End of table
+# Page(w, 'Template:Equip region table').edit(text=text[:start]+equipregions.main()+text[end:], summary=summary)
 
-import LODTables
-Page(w, 'Template:LODTable').edit(text=LODTables.main(), summary=summary)
+# import LODTables
+# Page(w, 'Template:LODTable').edit(text=LODTables.main(), summary=summary)
 
 import external_links_analyse2
 Page(w, 'Team Fortress Wiki:Reports/External links').edit(text=external_links_analyse2.main(), summary=summary)
