70342217d67c8cbcf3a183926427867f430f53ec
jbzdarkid <jbzdarkid@gmail.com>
2017-01-09 12:13:50 -0500

    Migrating to BOTzement, re-enabled scripts and will set daily run

--- master.py
+++ master.py
@@ -8,28 +8,28 @@ from os import environ
 w = wiki.Wiki('http://wiki.teamfortress.com/w/api.php')
 w.login(environ['WIKI_USERNAME'], environ['WIKI_PASSWORD'])
 
-summary = 'Automatic Update by %s using Travis-ci and [https://github.com/jbzdarkid/TFWiki-scripts Wikitools]' % environ['WIKI_USERNAME']
+summary = 'Automatic update using Travis-ci and https://github.com/jbzdarkid/TFWiki-scripts'
 
-# import wiki_edit_stats
-# Page(w, 'Team Fortress Wiki:Reports/Users by edit count').edit(text=wiki_edit_stats.main(), summary=summary)
+import wiki_edit_stats
+Page(w, 'Team Fortress Wiki:Reports/Users by edit count').edit(text=wiki_edit_stats.main(), summary=summary)
 
-# import wiki_undocumented_templates
-# Page(w, 'Team Fortress Wiki:Reports/Undocumented templates').edit(text=wiki_undocumented_templates.main(), summary=summary)
+import wiki_undocumented_templates
+Page(w, 'Team Fortress Wiki:Reports/Undocumented templates').edit(text=wiki_undocumented_templates.main(), summary=summary)
 
-# import wiki_unused_files
-# Page(w, 'Team Fortress Wiki:Reports/Unused files').edit(text=wiki_unused_files.main(), summary=summary)
+import wiki_unused_files
+Page(w, 'Team Fortress Wiki:Reports/Unused files').edit(text=wiki_unused_files.main(), summary=summary)
 
-# import equipregions
-# text = Page(w, 'Template:Equip region table').getWikiText()
-# start = text.index('! {{item name|') # Start of equip regions
-# end = text.index('<noinclude>') # End of table
-# Page(w, 'Template:Equip region table').edit(text=text[:start]+equipregions.main()+text[end:], summary=summary)
+import equipregions
+text = Page(w, 'Template:Equip region table').getWikiText()
+start = text.index('! {{item name|') # Start of equip regions
+end = text.index('<noinclude>') # End of table
+Page(w, 'Template:Equip region table').edit(text=text[:start]+equipregions.main()+text[end:], summary=summary)
 
-# import LODTables
-# Page(w, 'Template:LODTable').edit(text=LODTables.main(), summary=summary)
+import LODTables
+Page(w, 'Template:LODTable').edit(text=LODTables.main(), summary=summary)
 
-# import external_links_analyse2
-# Page(w, 'Team Fortress Wiki:Reports/External links').edit(text=external_links_analyse2.main(), summary=summary)
+import external_links_analyse2
+Page(w, 'Team Fortress Wiki:Reports/External links').edit(text=external_links_analyse2.main(), summary=summary)
 
 import wiki_untranslated_templates
 outputs = wiki_untranslated_templates.main()
