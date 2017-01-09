cfb5f4a9750796f18ec6a6998804e1e8f74f2b08
jbzdarkid <jbzdarkid@gmail.com>
2017-01-09 12:34:58 -0500

    Use a bot edit if possible

--- master.py
+++ master.py
@@ -11,27 +11,27 @@ w.login(environ['WIKI_USERNAME'], environ['WIKI_PASSWORD'])
 summary = 'Automatic update using Travis-ci and https://github.com/jbzdarkid/TFWiki-scripts'
 
 import wiki_edit_stats
-Page(w, 'Team Fortress Wiki:Reports/Users by edit count').edit(text=wiki_edit_stats.main(), summary=summary)
+Page(w, 'Team Fortress Wiki:Reports/Users by edit count').edit(text=wiki_edit_stats.main(), bot=True, summary=summary)
 
 import wiki_undocumented_templates
-Page(w, 'Team Fortress Wiki:Reports/Undocumented templates').edit(text=wiki_undocumented_templates.main(), summary=summary)
+Page(w, 'Team Fortress Wiki:Reports/Undocumented templates').edit(text=wiki_undocumented_templates.main(), bot=True, summary=summary)
 
 import wiki_unused_files
-Page(w, 'Team Fortress Wiki:Reports/Unused files').edit(text=wiki_unused_files.main(), summary=summary)
+Page(w, 'Team Fortress Wiki:Reports/Unused files').edit(text=wiki_unused_files.main(), bot=True, summary=summary)
 
 # import equipregions
 # text = Page(w, 'Template:Equip region table').getWikiText()
 # start = text.index('! {{item name|') # Start of equip regions
 # end = text.index('<noinclude>') # End of table
-# Page(w, 'Template:Equip region table').edit(text=text[:start]+equipregions.main()+text[end:], summary=summary)
+# Page(w, 'Template:Equip region table').edit(text=text[:start]+equipregions.main()+text[end:], bot=True, summary=summary)
 
 # import LODTables
-# Page(w, 'Template:LODTable').edit(text=LODTables.main(), summary=summary)
+# Page(w, 'Template:LODTable').edit(text=LODTables.main(), bot=True, summary=summary)
 
 import external_links_analyse2
-Page(w, 'Team Fortress Wiki:Reports/External links').edit(text=external_links_analyse2.main(), summary=summary)
+Page(w, 'Team Fortress Wiki:Reports/External links').edit(text=external_links_analyse2.main(), bot=True, summary=summary)
 
 import wiki_untranslated_templates
 outputs = wiki_untranslated_templates.main()
 for lang, output in outputs:
-  Page(w, 'Team Fortress Wiki:Reports/Untranslated templates/%s' % lang).edit(output, summary=summary)
+  Page(w, 'Team Fortress Wiki:Reports/Untranslated templates/%s' % lang).edit(output, bot=True, summary=summary)
