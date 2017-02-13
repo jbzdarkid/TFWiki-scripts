1a7428a1fa6de5c71640094747d20f197e84b605
jbzdarkid <jbzdarkid@gmail.com>
2017-02-12 23:34:56 -0500

    Moving and removing the uneeded files

--- master.py
+++ master.py
@@ -17,15 +17,6 @@ print Page(w, 'Team Fortress Wiki:Reports/Users by edit count').edit(text=edit_s
 import undocumented_templates
 print Page(w, 'Team Fortress Wiki:Reports/Undocumented templates').edit(text=undocumented_templates.main(), bot=True, summary=summary)
 
-# import equipregions
-# text = Page(w, 'Template:Equip region table').getWikiText()
-# start = text.index('! {{item name|') # Start of equip regions
-# end = text.index('<noinclude>') # End of table
-# print Page(w, 'Template:Equip region table').edit(text=text[:start]+equipregions.main()+text[end:], bot=True, summary=summary)
-
-# import LODTables
-# print Page(w, 'Template:LODTable').edit(text=LODTables.main(), bot=True, summary=summary)
-
 import external_links_analyse2
 print Page(w, 'Team Fortress Wiki:Reports/External links').edit(text=external_links_analyse2.main(), bot=True, summary=summary)
 
