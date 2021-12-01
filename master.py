d8756885b35cdb20f64d2774991f3b4becba5dbd
Joseph Blackman <joblac@microsoft.com>
2021-12-01 05:54:25 -0800

    I hope this works...

--- master.py
+++ master.py
@@ -2,27 +2,29 @@ from wikitools.page import Page
 from wikitools import wiki
 from os import environ
 
-# travis encrypt WIKI_USERNAME=pootis -a -x
-# travis encrypt WIKI_PASSWORD=woooki -a -p
-
 w = wiki.Wiki('http://wiki.teamfortress.com/w/api.php')
 w.login(environ['WIKI_USERNAME'], environ['WIKI_PASSWORD'])
 
-summary = 'Automatic update using Travis-ci and https://github.com/jbzdarkid/TFWiki-scripts'
+summary = 'Automatic update via https://github.com/jbzdarkid/TFWiki-scripts'
+
+if True: # TODO: Disable testing once I think it works.
+  root = 'User:Darkid/Reports'
+else:
+  root = 'Team Fortress Wiki:Reports'
 
 import edit_stats
-print(Page(w, 'Team Fortress Wiki:Reports/Users by edit count').edit(text=edit_stats.main(), bot=True, summary=summary))
+print(Page(w, f'{root}/Users by edit count').edit(text=edit_stats.main(), bot=True, summary=summary))
 
 import external_links
-print(Page(w, 'Team Fortress Wiki:Reports/External links').edit(text=external_links.main(), bot=True, summary=summary))
+print(Page(w, f'{root}/External links').edit(text=external_links.main(), bot=True, summary=summary))
 
 import undocumented_templates
-print(Page(w, 'Team Fortress Wiki:Reports/Undocumented templates').edit(text=undocumented_templates.main(), bot=True, summary=summary))
+print(Page(w, f'{root}/Undocumented templates').edit(text=undocumented_templates.main(), bot=True, summary=summary))
 
 import untranslated_templates
 outputs = untranslated_templates.main()
 for lang, output in outputs:
-  print(Page(w, 'Team Fortress Wiki:Reports/Untranslated templates/%s' % lang).edit(output, bot=True, summary=summary))
+  print(Page(w, f'{root}/Untranslated templates/{lang}').edit(output, bot=True, summary=summary))
   
-  import unused_files
-print(Page(w, 'Team Fortress Wiki:Reports/Unused files').edit(text=unused_files.main(), bot=True, summary=summary))
+import unused_files
+print(Page(w, f'{root}/Unused files').edit(text=unused_files.main(), bot=True, summary=summary))
