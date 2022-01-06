f0c7c8540ea7b8fb260cb4cf28069cf5321fb984
Joseph Blackman <joblac@microsoft.com>
2022-01-06 09:25:44 -0800

    Well, okay then

--- master.py
+++ master.py
@@ -13,15 +13,6 @@ from wikitools.page import Page
 # update readme (again)
 # Improve the wikitools/wiki get_with_continue to actually yield the pagenames, not just the json objects.
 
-with open(environ['GITHUB_ENV'], 'a') as f:
-  subcomment = """\
-Please verify the following diffs:
-- [ ] All articles ran in 0:00:56.697371: [en](https://wiki.tf/d/3106540) [ar](https://wiki.tf/d/3106518) [cs](https://wiki.tf/d/3106519) [da](https://wiki.tf/d/3106520) [de](https://wiki.tf/d/3106521) [es](https://wiki.tf/d/3106522) [fi](https://wiki.tf/d/3106523) [fr](https://wiki.tf/d/3106524) [hu](https://wiki.tf/d/3106525) [it](https://wiki.tf/d/3106526) [ja](https://wiki.tf/d/3106527) [ko](https://wiki.tf/d/3106528) [nl](https://wiki.tf/d/3106529) [no](https://wiki.tf/d/3106530) [pl](https://wiki.tf/d/3106531) [pt](https://wiki.tf/d/3106532) [pt-br](https://wiki.tf/d/3106533) [ro](https://wiki.tf/d/3106534) [ru](https://wiki.tf/d/3106535) [sv](https://wiki.tf/d/3106536) [tr](https://wiki.tf/d/3106537) [zh-hans](https://wiki.tf/d/3106538) [zh-hant](https://wiki.tf/d/3106539)
-- [ ] Wanted templates ran in 0:00:37.264911: [en](https://wiki.tf/d/3106541)"""
-  f.write('GITHUB_COMMENT<<EOF\n{subcomment}\nEOF\n')
-
-exit(0)
-
 diff_links = []
 
 def publish_single_report(w, module, report_name):
@@ -110,6 +101,6 @@ if __name__ == '__main__':
 
   # Pass this as output to github-actions, so it can be used in later steps
   with open(environ['GITHUB_ENV'], 'a') as f:
-    f.write('GITHUB_COMMENT<<EOF{comment}EOF')
+    f.write('GITHUB_COMMENT<<EOF\n{comment}\nEOF\n')
 
   exit(failures)
