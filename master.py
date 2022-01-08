4034551a85836b774fe2b22236b7d95630553e94
Joseph Blackman <joblac@microsoft.com>
2022-01-08 03:28:50 -0800

    Uh, maybe

--- master.py
+++ master.py
@@ -14,26 +14,19 @@ from wikitools.page import Page
 # update readme (again)
 # Improve the wikitools/wiki get_with_continue to actually yield the pagenames, not just the json objects.
 
-diff_links = []
-
 def publish_report(w, module, report_name):
-  start = datetime.now()
   try:
     report_output = importlib.import_module(module).main(w)
-    diff_link_map = {}
 
     if isinstance(report_output, list):
       for lang, output in report_output:
         diff_link_map[lang] = Page(w, f'{root}/{report_name}/{lang}').edit(output, bot=True, summary=summary)
     else:
       diff_link_map['en'] = Page(w, f'{root}/{report_name}').edit(output, bot=True, summary=summary)
-
-    diff_links.append((report_name, datetime.now() - start, diff_link_map))
-    return 0
+    return diff_link_map
   except Exception:
     print(f'Failed to update {report_name}')
     print_exc(file=stdout)
-    return 1
 
 all_reports = {
   'untranslated_templates': 'Untranslated templates',
@@ -78,44 +71,28 @@ if __name__ == '__main__':
   w = wiki.Wiki('https://wiki.teamfortress.com/w/api.php')
   if not w.login(environ['WIKI_USERNAME'], environ['WIKI_PASSWORD']):
     exit(1)
-  failures = 0
-
-  for report in lang_reports:
-    failures += publish_lang_report(w, report['file'], report['title'])
-  for report in single_reports:
-    failures += publish_single_report(w, report['file'], report['title'])
-
-    failures += publish_lang_report(w, 'untranslated_templates', 'Untranslated templates')
-    failures += publish_lang_report(w, 'missing_translations', 'Missing translations')
-    failures += publish_lang_report(w, 'missing_categories', 'Untranslated categories')
-    failures += publish_lang_report(w, 'all_articles', 'All articles')
-
-  if is_weekly: # English (or cross-language) reports which are not too costly to run
-    failures += publish_single_report(w, 'wanted_templates', 'Wanted templates')
-    failures += publish_single_report(w, 'navboxes', 'Pages which are missing navboxes')
-    failures += publish_single_report(w, 'overtranslated', 'Pages with no english equivalent')
-
-  if is_monthly: # Expensive or otherwise infrequently-changing reports
-    failures += publish_single_report(w, 'incorrectly_categorized', 'Pages with incorrect categorization')
-    failures += publish_single_report(w, 'duplicate_files', 'Duplicate files')
-    failures += publish_single_report(w, 'unused_files', 'Unused files')
-    failures += publish_single_report(w, 'undocumented_templates', 'Undocumented templates')
-    failures += publish_single_report(w, 'edit_stats', 'Users by edit count')
-    failures += publish_single_report(w, 'external_links', 'External links')
-    failures += publish_single_report(w, 'mismatched', 'Mismatched parenthesis')
-    failures += publish_single_report(w, 'displaytitles', 'Duplicate displaytitles')
 
   comment = 'Please verify the following diffs:\n'
-  for report_name, duration, link_map in diff_links:
-    duration -= timedelta(microseconds=duration.microseconds) # Strip microseconds
-    comment += f'- [ ] {report_name} ran in {duration}:'
-    languages = sorted(link_map.keys(), key=lambda lang: (lang != 'en', lang)) # Sort languages, keeping english first
-    for language in languages:
-      comment += f' [{language}]({link_map[language]})'
-    comment += '\n'
+  suceeded = True
+
+  for report in reports_to_run:
+    start = datetime.now()
+    diff_link_map = publish_report(w, report, all_reports[report])
+    duration = datetime.now() - start
+    if not diff_link_map:
+      action_url = 'https://github.com/' + environ['GITHUB_REPOSITORY'] + '/runs/' + environ['GITHUB_ACTION']
+      comment += f'- [ ] {report_name} failed after {duration}: {action_url}\n'
+      succeeded = False
+    else:
+      comment += f'- [ ] {report_name} succeeded in {duration}:'
+      languages = sorted(link_map.keys(), key=lambda lang: (lang != 'en', lang)) # Sort languages, keeping english first
+      for language in languages:
+        comment += f' [{language}]({link_map[language]})'
+      comment += '\n'
 
   # Pass this as output to github-actions, so it can be used in later steps
+  # Or, like, import the other file and just call it. IDK.
   with open(environ['GITHUB_ENV'], 'a') as f:
     f.write(f'GITHUB_COMMENT<<EOF\n{comment}\nEOF\n')
 
-  exit(failures)
+  exit(0 if succeeded else 1)
