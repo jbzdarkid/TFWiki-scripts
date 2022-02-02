1333b48325521daf73c3be45a9221d5c506fd15a
Joseph Blackman <joblac@microsoft.com>
2022-02-01 19:56:41 -0800

    Code linter bugs and real bugs

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ from queue import Empty, Queue
 from threading import Thread, Event
 
 class meta_plural(type):
-  def __getattr__(clazz, word):
+  def __getattr__(cls, word):
     if word.endswith('s'):
       word = word[:-1]
     return lambda num: f'1 {word}' if num == 1 else f'{num} {word}s'
@@ -23,19 +23,19 @@ class pagescraper_queue:
   NUM_THREADS = 50
 
   def __init__(self, thread_func, *args):
-    self.q = Queue()
-    self.done = Event()
-    self.threads = []
     self.thread_func = thread_func
     self.thread_func_args = args
 
   def __enter__(self):
-    for _ in range(PAGESCRAPERS): # Number of threads
+    self.q = Queue()
+    self.done = Event()
+    self.threads = []
+    for _ in range(NUM_THREADS): # Number of threads
       thread = Thread(target=self.meta_thread_func)
-      threads.append(thread)
+      self.threads.append(thread)
       thread.start()
 
-  def __exit__(self):
+  def __exit__(self, exc_type, exc_val, traceback)
     self.done.set()
     for thread in self.threads:
       thread.join()
@@ -46,7 +46,7 @@ class pagescraper_queue:
   def meta_thread_func(self):
     while True:
       try:
-        obj = self.get(True, 1)
+        obj = self.q.get(True, 1)
       except Empty:
         if self.done.is_set():
           return
