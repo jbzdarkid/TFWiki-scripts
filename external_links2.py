d8e864a0d64faddf3f1f675b7da78372db9248e4
Joseph Blackman <joblac@microsoft.com>
2022-07-13 22:54:51 -0700

    limit 500

--- external_links2.py
+++ external_links2.py
@@ -58,24 +58,24 @@ def safely_request(verb, url, timeout=20):
   return None # no error
 
 def domain_verifier(domains, dead_domains, dangerous_domains):
-  assert (len(domains) < 500)
-  json = {
-    'client': {'clientId': 'github.com/jbzdarkid/TFWiki-scripts', 'clientVersion': '1.0'},
-    'threatInfo': {
-      'threatTypes': ['MALWARE', 'SOCIAL_ENGINEERING', 'UNWANTED_SOFTWARE', 'POTENTIALLY_HARMFUL_APPLICATION'],
-      'platformTypes': ['ANY_PLATFORM'],
-      'threatEntryTypes': ['URL'],
-      'threatEntries': [{'url': domain} for domain in domains],
+  for i in range(0, len(domains), 500): # We can only request 500 domains at a time.
+    json = {
+      'client': {'clientId': 'github.com/jbzdarkid/TFWiki-scripts', 'clientVersion': '1.0'},
+      'threatInfo': {
+        'threatTypes': ['MALWARE', 'SOCIAL_ENGINEERING', 'UNWANTED_SOFTWARE', 'POTENTIALLY_HARMFUL_APPLICATION'],
+        'platformTypes': ['ANY_PLATFORM'],
+        'threatEntryTypes': ['URL'],
+        'threatEntries': [{'url': domain} for domain in domains[i:i+500],
+      }
     }
-  }
 
-  r = requests.post('https://safebrowsing.googleapis.com/v4/threatMatches:find?key=' + environ['API_KEY'], json=json)
-  print(r.text)
-  j = r.json()
-  if matches := j.get('matches'):
-    for match in matches:
-      domain = match['threat']['url']
-      dangerous_domains[domain] = match['threatType'].replace('_', ' ').title()
+    r = requests.post('https://safebrowsing.googleapis.com/v4/threatMatches:find?key=' + environ['API_KEY'], json=json)
+    print(r.text)
+    j = r.json()
+    if matches := j.get('matches'):
+      for match in matches:
+        domain = match['threat']['url']
+        dangerous_domains[domain] = match['threatType'].replace('_', ' ').title()
 
 def link_verifier(domain_links, dead_links):
   for link in domain_links:
