816a12feeaa69dbf070a8cf652d7647f797d7970
Joseph Blackman <joblac@microsoft.com>
2022-11-02 08:53:45 -0700

    Small fixes

--- master.py
+++ master.py
@@ -27,13 +27,13 @@ import open_pr_comment
 # Threading for navboxes.py?
 # Ensure that PRs which add files also touch readme.md
 
-def edit_or_save(page_name, file_name, output):
+def edit_or_save(page_name, file_name, output, summary):
   wiki_diff_url = Page(w, page_name).edit(output, bot=True, summary=summary)
   if wiki_diff_url:
     return wiki_diff_url
 
   with open(file_name, 'w', encoding='utf-8') as f:
-    f.write(report_output)
+    f.write(output)
 
   return None
 
@@ -44,9 +44,9 @@ def publish_report(w, module, report_name, root, summary):
 
     if isinstance(report_output, list):
       for lang, output in report_output:
-        link_map[lang] = edit_or_save(f'{root}/{report_name}/{lang}', f'wiki_{report_name}_{lang}.txt', output)
+        link_map[lang] = edit_or_save(f'{root}/{report_name}/{lang}', f'wiki_{report_name}_{lang}.txt', output, summary)
     else:
-      link_map['en'] = edit_or_save(f'{root}/{report_name}', f'wiki_{report_name}.txt', report_output)
+      link_map['en'] = edit_or_save(f'{root}/{report_name}', f'wiki_{report_name}.txt', report_output, summary)
 
   except Exception:
     print(f'Failed to update {report_name}')
