89ca82f32930085d859ffb59e5cc79d7d8f7e5cf
jbzdarkid <jbzdarkid@gmail.com>
2024-10-13 22:32:09 -0700

    Working finally

--- unlicensed_images.py
+++ unlicensed_images.py
@@ -47,11 +47,6 @@ def main(w):
 
   files_with_multiple_templates = [file for file, templates in all_files.items() if len(templates) > 1]
   files_with_no_template =        [file for file, templates in all_files.items() if len(templates) == 0]
-  
-  print(len(non_files_with_transclusions))
-  print(len(files_with_multiple_templates))
-  print(len(files_with_no_template))
-  raise
 
   output = """\
 {{{{DISPLAYTITLE: {count} files with incorrect licensing}}}}
