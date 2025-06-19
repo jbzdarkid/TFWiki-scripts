1b1f51ae2cef39a7430c419923d74c423d136429
Joseph Blackman <jbzdarkid@users.noreply.github.com>
2025-06-19 07:33:06 -0700

    Switch back to old parser ig

--- reports/untranslated_templates.py
+++ reports/untranslated_templates.py
@@ -9,6 +9,15 @@ from datetime import datetime
 verbose = False
 LANGS = ['ar', 'cs', 'da', 'de', 'es', 'fi', 'fr', 'hu', 'it', 'ja', 'ko', 'nl', 'no', 'pl', 'pt', 'pt-br', 'ro', 'ru', 'sv', 'tr', 'zh-hans', 'zh-hant']
 
+LANG_TEMPLATE_START = compile(r"""
+  [^{]{{            # The start of a template '{{' which is not the start of a parameter '{{{'
+  \s*               # Any amount of whitespace is allowed before the template name
+  lang              # Template name {{lang}}
+  ( incomplete)?    # Also matches {{lang incomplete}} but we can check which one it is by the first group
+  \s*               # Any amount of whitespace (but critically, no more ascii characters)
+  \|                # Start of parameter list
+""", IGNORECASE | VERBOSE)
+
 LANG_TEMPLATE_ARGS = compile(r"""
   \|        # Start of a parameter
   (
@@ -20,28 +29,52 @@ LANG_TEMPLATE_ARGS = compile(r"""
   )
 """, VERBOSE)
 
+
 def parse_lang_templates(page):
   counters[0] -= datetime.now().timestamp()
   page_text = page.get_wiki_text()
   counters[0] += datetime.now().timestamp()
 
-  counters[1] -= datetime.now().timestamp()
-  buffer = {0: ''} # Text buffers for each level of the template, i.e. {{contains this text {{but not this text}} and still this text}}
-  stack = [0] # Contains the indices which open the stack depth(s), i.e. the hierarchy of nested templates
-  for i, char in enumerate(page_text):
-    if char in '{[':
-      stack.append(i)
-      continue
-    elif char in '}]':
-      # The 'base' of the stack should not ever be popped (since it represents text at root scope)
-      # If we get a mismatch just... do our best. Mismatched parens will be reported elsewhere.
-      if len(stack) > 1:
-        stack.pop() 
-      continue
+  # First, find the matching pairs
+  def get_indices(char, string):
+    index = -1
+    indices = []
+    while 1:
+      try:
+        index = string.index(char, index+1)
+      except ValueError:
+        break
+      indices.append(index)
+    return indices
 
-    # Add this character to the buffer for the current stack (or create the buffer if it doesn't exist)
-    buffer[stack[-1]] = buffer.get(stack[-1], '') + char
-  counters[1] += datetime.now().timestamp()
+  locations = [[len(page_text), -1]]
+  for open in get_indices('{', page_text):
+    locations.append([open, 1])
+  for open in get_indices('[', page_text):
+    locations.append([open, 1])
+  for close in get_indices('}', page_text):
+    locations.append([close, -1])
+  for close in get_indices(']', page_text):
+    locations.append([close, -1])
+  locations.sort()
+
+  # Next, divide the text up based on those pairs. Embedded text is separated out, e.g. {a{b}c} will become "ac" and "b".
+  stack = [0]
+  buffer = {0: ''}
+  lastIndex = 0
+  for index, value in locations:
+    try:
+      buffer[stack[-1]] = buffer.get(stack[-1], '') + page_text[lastIndex:index]
+    except IndexError:
+      buffer[0] += page_text[lastIndex:index] # Add text to default layer
+      stack.append(None) # So there's something to .pop()
+      if verbose:
+        print(page.title, 'Found a closing brace without a matched opening brace')
+    if value == 1:
+      stack.append(index)
+    elif value == -1:
+      stack.pop()
+    lastIndex = index + 1
 
   if verbose:
     print(page.title, 'contains', len(buffer), 'pairs of braces')
@@ -49,33 +82,22 @@ def parse_lang_templates(page):
   # Finally, search through for lang templates using regex
   lang_templates = []
 
-  counters[2] -= datetime.now().timestamp()
-  for index, text in buffer.items():
-    template_name = text.split('|', 1)[0].strip()
-    if not template_name.startswith('lang'):
-      continue # We only care about {{lang}} and {{lang incomplete}}
+  for match in LANG_TEMPLATE_START.finditer(page_text):
+    lang_template = {'args': []}
+    for match2 in LANG_TEMPLATE_ARGS.finditer(buffer[match.start() + 2]): # Skip the opening {{
+      language = match2.group(1).strip().lower()
+      text = match2.group(2).strip()
+      lang_template['args'].append((language, text))
 
-    args = []
-    first_arg_text = ''
-    counters[3] -= datetime.now().timestamp()
-    for match in LANG_TEMPLATE_ARGS.finditer(text):
-      language = match.group(1).strip().lower()
-      text = match.group(2).strip()
-      args.append((language, text)) # Note that we're not using a dictionary here since some consumers care about duplicates
-      if not first_arg_text:
-        first_arg_text = text.split('\n', 1)[0].strip()
-    counters[3] += datetime.now().timestamp()
-    if not first_arg_text or first_arg_text == '':
-      print(args)
+    lang_template['location'] = "''Line %d'': <nowiki>%s</nowiki>" % (
+      page_text[:match.start()].count('\n') + 1,
+      lang_template['args'][0][1].split('\n', 1)[0].strip() if len(lang_template['args']) > 0 else '',
+    )
 
-    line_no = page_text[:index].count('\n') + 1
-    lang_templates.append({
-      'template': template_name,
-      'args': args,
-      'location': f"''Line {line_no}'': <nowiki>{first_arg_text}</nowiki>",
-    })
+    lang_template['template'] = match.group(1)
 
-  counters[2] += datetime.now().timestamp()
+    lang_templates.append(lang_template)
+
   return lang_templates
 
 def pagescraper(page, translations, usage_counts):
