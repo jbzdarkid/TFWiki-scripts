0c41407bf1860d8bd0f64236ef02d73b81693b21
Joseph Blackman <jbzdarkid@users.noreply.github.com>
2025-06-19 12:02:26 -0700

    testing

--- reports/untranslated_templates.py
+++ reports/untranslated_templates.py
@@ -3,10 +3,7 @@ from .utils import pagescraper_queue, time_and_date, plural, whatlinkshere
 from wikitools import wiki
 from wikitools.page import Page
 
-counters = [0] * 100
-from datetime import datetime
-
-verbose = False
+verbose = True
 LANGS = ['ar', 'cs', 'da', 'de', 'es', 'fi', 'fr', 'hu', 'it', 'ja', 'ko', 'nl', 'no', 'pl', 'pt', 'pt-br', 'ro', 'ru', 'sv', 'tr', 'zh-hans', 'zh-hant']
 
 LANG_TEMPLATE_START = compile(r"""
@@ -30,10 +27,58 @@ LANG_TEMPLATE_ARGS = compile(r"""
 """, VERBOSE)
 
 
-def parse_lang_templates(page):
-  counters[0] -= datetime.now().timestamp()
+def parse_lang_templates2(page):
   page_text = page.get_wiki_text()
-  counters[0] += datetime.now().timestamp()
+
+  buffer = {0: ''} # Text buffers for each level of the template, i.e. {{contains this text {{but not this text}} and still this text}}
+  stack = [0] # Contains the indices which open the stack depth(s), i.e. the hierarchy of nested templates
+  for i, char in enumerate(page_text):
+    if char in '{[':
+      stack.append(i)
+      continue
+    elif char in '}]':
+      # The 'base' of the stack should not ever be popped (since it represents text at root scope)
+      # If we get a mismatch just... do our best. Mismatched parens will be reported elsewhere.
+      if len(stack) > 1:
+        stack.pop() 
+      continue
+
+    # Add this character to the buffer for the current stack (or create the buffer if it doesn't exist)
+    buffer[stack[-1]] = buffer.get(stack[-1], '') + char
+
+  if verbose:
+    print(page.title, 'contains', len(buffer), 'pairs of braces')
+
+  # Finally, search through for lang templates using regex
+  lang_templates = []
+
+  for index, text in buffer.items():
+    template_name = text.split('|', 1)[0].strip()
+    if not template_name.startswith('lang'):
+      continue # We only care about {{lang}} and {{lang incomplete}}
+
+    print(page.title, template_name, index, text)
+
+    args = []
+    first_arg_text = ''
+    for match in LANG_TEMPLATE_ARGS.finditer(text):
+      language = match.group(1).strip().lower()
+      text = match.group(2).strip()
+      args.append((language, text)) # Note that we're not using a dictionary here since some consumers care about duplicates
+      if not first_arg_text:
+        first_arg_text = text.split('\n', 1)[0].strip()
+
+    line_no = page_text[:index].count('\n') + 1
+    lang_templates.append({
+      'template': template_name,
+      'args': args,
+      'location': f"''Line {line_no}'': <nowiki>{first_arg_text}</nowiki>",
+    })
+
+  return lang_templates
+
+def parse_lang_templates(page):
+  page_text = page.get_wiki_text()
 
   # First, find the matching pairs
   def get_indices(char, string):
@@ -83,12 +128,14 @@ def parse_lang_templates(page):
   lang_templates = []
 
   for match in LANG_TEMPLATE_START.finditer(page_text):
+    print(page.title, match.group(1), match.start() + 2, buffer[match.start() + 2])
+
     lang_template = {'args': []}
     for match2 in LANG_TEMPLATE_ARGS.finditer(buffer[match.start() + 2]): # Skip the opening {{
       language = match2.group(1).strip().lower()
       text = match2.group(2).strip()
       lang_template['args'].append((language, text))
-    print(lang_template['args']) # TODO: Testing
+
     lang_template['location'] = "''Line %d'': <nowiki>%s</nowiki>" % (
       page_text[:match.start()].count('\n') + 1,
       lang_template['args'][0][1].split('\n', 1)[0].strip() if len(lang_template['args']) > 0 else '',
@@ -101,14 +148,11 @@ def parse_lang_templates(page):
   return lang_templates
 
 def pagescraper(page, translations, usage_counts):
-  counters[4] -= datetime.now().timestamp()
   lang_templates = parse_lang_templates(page)
-  counters[4] += datetime.now().timestamp()
 
   if len(lang_templates) == 0:
     return # Should be impossible (since we're looking for templates which transclude {{lang}}), but just in case.
 
-  counters[5] -= datetime.now().timestamp()
   missing_translations = {lang:[] for lang in LANGS}
   for lang_template in lang_templates:
     if lang_template['template'] == 'lang incomplete':
@@ -121,14 +165,11 @@ def pagescraper(page, translations, usage_counts):
 
     for language in missing_languages:
       missing_translations[language].append(location)
-  counters[5] += datetime.now().timestamp()
 
   if len(missing_translations) == 0:
     return # Template is fully translated, no need to report on it for anyone.
 
-  counters[6] -= datetime.now().timestamp()
   usage_count = page.get_transclusion_count()
-  counters[6] += datetime.now().timestamp()
 
   if usage_count == 0:
     return # Who cares, if it's not being used.
@@ -142,25 +183,21 @@ def pagescraper(page, translations, usage_counts):
 def main(w):
   translations = {lang: [] for lang in LANGS}
   usage_counts = {}
-  counters[7] -= datetime.now().timestamp()
-  counters[9] -= datetime.now().timestamp()
-  # For performance, only search for templates which are reported to transclude lang/lang incomplete.
-  pages_with_lang = set()
-  pages_with_lang.update(Page(w, 'Template:Lang').get_transclusions(namespaces=['Template']))
-  pages_with_lang.update(Page(w, 'Template:Lang incomplete').get_transclusions(namespaces=['Template']))
-  print(pages_with_lang)
-  counters[9] += datetime.now().timestamp()
-
+  i = 0
   with pagescraper_queue(pagescraper, translations, usage_counts) as pages:
+    # For performance, only check template pages which are reported to transclude lang/lang incomplete.
+    pages_with_lang = set()
+    pages_with_lang.update(Page(w, 'Template:Lang').get_transclusions(namespaces=['Template']))
+    pages_with_lang.update(Page(w, 'Template:Lang incomplete').get_transclusions(namespaces=['Template']))
     for page in pages_with_lang:
-      if '/' in page.title:
-        continue # Don't include subpage templates like Template:Dictionary and Template:PatchDiff
       if page.title[:13] == 'Template:User':
         continue # Don't include userboxes
       if page.title == 'Template:Lang':
         continue # Special exclusion
       pages.put(page)
-  counters[7] += datetime.now().timestamp()
+      i += 1
+      if i > 10:
+        break
 
   outputs = []
   for language in LANGS:
@@ -178,16 +215,13 @@ Pages missing in {{{{lang info|{lang}}}}}: '''<onlyinclude>{count}</onlyinclude>
       count=len(translations[language]),
       date=time_and_date())
 
-    counters[8] -= datetime.now().timestamp()
     for template, missing in sorted(translations[language], key=lambda elem: (-usage_counts[elem[0].title], elem[0].title)):
       count = usage_counts[template.title]
       output += f'\n# [{template.get_edit_url()} {template.title}] has [{whatlinkshere(template.title, count)} {plural.uses(count)}] and is missing {plural.translations(len(missing))}'
       for location in missing:
         output += f'\n#:{location}'
-    counters[8] += datetime.now().timestamp()
     outputs.append([language, output])
 
-  print('Counters', counters)
   return outputs
 
 if __name__ == '__main__':
