3919cc3c5e1ac93c2e6a9b80fbd517a4c9082f7a
Joseph Blackman <jbzdarkid@users.noreply.github.com>
2025-06-20 20:55:41 +0000

    Also updated tests

--- tests.py
+++ tests.py
@@ -1,6 +1,37 @@
 # A very light smattering of tests
 import inspect
 import sys
+from wikitools.wiki import Wiki
+from wikitools.page import Page
+from reports.untranslated_templates import parse_lang_templates, parse_lang_templates2
+
+class MockWiki(Wiki):
+  def get_namespaces(self):
+    return {}
+
+
+raw_text = """{{lang incomplete
+| en = (
+| ja = （
+| zh-hans = （
+| zh-hant = （
+}}{{{1}}}{{lang incomplete
+| en = )
+| ja = ）
+| zh-hans = ）
+| zh-hant = ）
+}}<noinclude>
+
+{{Doc begin}}
+== Usage ==
+:<code><nowiki>{{Parenthesis|1}}</nowiki></code>
+Replace "1" with the message you need in parenthesis. Localised for Japanese and Traditional/Simplified Chinese. Otherwise uses standard English parentheses.
+
+== Example ==
+{{tlx|Parenthesis|Hello Wiki!}} produces {{Parenthesis|Hello Wiki!}}
+
+[[Category:Formatting templates|Parenthesis]]
+</noinclude>"""
 
 class Tests:
   # Class setup
@@ -9,9 +40,19 @@ class Tests:
   #!# Tests #!#
   #############
 
-  # TODO: Define tests here (which start with the word test)
-  def test_nothing(self):
-    pass
+  def test_lang_parser(self):
+    w = MockWiki('https://wiki.teamfortress.com/w/api.php')
+    p = Page(w, 'Template:Parenthesis')
+    for text in [raw_text]:
+      w.page_text_cache[p.title] = text
+      l1 = parse_lang_templates(p)
+      l2 = parse_lang_templates2(p)
+      assert len(l1) == len(l2), f'{len(l1)} != {len(l2)}\n{l1}\n{l2}'
+      for i in range(len(l1)):
+        assert l1[i]['template'] == l2[i]['template'], f'{l1[i]["template"]} != {l2[i]["template"]}'
+        assert l1[i]['location'] == l2[i]['location'], f'{l1[i]["location"]}\n!=\n{l2[i]["location"]}'
+        assert l1[i]['args'] == l2[i]['args']
+
 
 if __name__ == '__main__':
   tests = Tests()
