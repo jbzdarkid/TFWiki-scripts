374093d60aeedd226c5b3f44d0f01d3f515fc030
Joseph Blackman <jbzdarkid@gmail.com>
2025-10-09 22:31:52 -0700

    oops

--- debug_cache.py
+++ debug_cache.py
@@ -6,7 +6,8 @@ w = Wiki('https://wiki.teamfortress.com/w/api.php')
 if len(sys.argv) > 1:
   print(w.page_text_cache[sys.argv[1]])
   print(w.page_text_cache.metadata[sys.argv[1]])
-  w.update_caches_from_recent_changes(self, days_ago=30)
+  print('-' * 100)
+  w.update_caches_from_recent_changes(days_ago=30)
   print(w.page_text_cache[sys.argv[1]])
   print(w.page_text_cache.metadata[sys.argv[1]])
 
