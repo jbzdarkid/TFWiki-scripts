e6ae7400a330c70746cf052e8a5ca2b4a53e1632
Joseph Blackman <jbzdarkid@users.noreply.github.com>
2026-09-25 09:02:38 -0700

    Added although I think it's not really working

---
+++ reports/bad_links.py
@@ -0,0 +1,71 @@
+from collections import defaultdict
+from re import finditer
+from .utils import pagescraper_queue_single, time_and_date
+from wikitools import wiki
+
+verbose = False
+
+def pagescraper(page, links, anchors):
+  html = page.get_raw_html()
+  if len(html) == 142:
+    return
+  if 'a href' in html:
+    lines = html.split('\n')
+    for line in lines:
+      if 'a href' in line:
+        print(line)
+  for m in finditer('<a href="/wiki/(.*?)#(.*?)"', html):
+    target = m.group(1)
+    section = m.group(2)
+    print(target, section)
+    links[target][section].append(page)
+
+  for m in finditer('<span .*?id="(.*?)"', html):
+    anchors[page].append(m.group(1))
+
+def main(w):
+  links = defaultdict(lambda: defaultdict(list))
+  anchors = defaultdict(list)
+
+  with pagescraper_queue_single(pagescraper, links, anchors) as pages:
+    i = 0
+    for page in w.get_all_pages(namespaces=['Main', 'TFW', 'File', 'Template', 'Help', 'Category']):
+      pages.put(page)
+      i += 1
+      if i > 100:
+        break
+      input()
+
+  print(links)
+  print(anchors)
+  broken_links = defaultdict(lambda: defaultdict(list))
+  for target in links:
+    for section in links[target]:
+      if section in anchors[target]:
+        for source in links[target][section]:
+          broken_links[source.lang][source].append((target, section))
+
+  output = """\
+{{{{DISPLAYTITLE: {count} pages with broken subsection links}}}}
+<onlyinclude>{count}</onlyinclude> pages which link to a nonexistant subsection on another page. Data as of {date}.
+
+""".format(
+    count=sum(len(v) for v in broken_links.values()),
+    date=time_and_date())
+
+
+  for lang in broken_links:
+    output += '== {{lang name|name|%s}} ==\n' % lang
+    for page in sorted(broken_links[lang]):
+      output += f'=== [[{page.title}]] ===\n'
+      for target, section in broken_links[lang][page]:
+        output += f'* [[{target}#{section}]]'
+
+  return output
+
+if __name__ == '__main__':
+  verbose = True
+  w = wiki.Wiki('https://wiki.teamfortress.com/w/api.php')
+  with open('wiki_bad_links.txt', 'w', encoding='utf-8') as f:
+    f.write(main(w))
+  print(f'Article written to {f.name}')
